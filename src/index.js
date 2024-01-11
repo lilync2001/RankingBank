9e7abaacbcc3edc11ba9303f1ae7931c34d64059
Jahel Nogales <jahel.nogales@espoch.edu.ec>
2024-01-11 10:47:24 -0500

    Documentación

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import app from "./app.js";
+import { sequelize } from "./database/database.js";
+async function main() {
+    try {
+        try {
+            await sequelize.sync({ force: false, logging: console.log });
+            console.log('Conexión exitosa');
+            app.listen(3000);
+            console.log('Servidor escuchando en el puerto', 3000);
+        } catch (error) {
+            console.log(error.message)
+        }
+    } catch (error) {
+        console.log('ERROR!, No se puede conectar a la base de datos');
+    }
+}
+
+main();
